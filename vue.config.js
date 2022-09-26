01478a427d78249383d5e9ff10ac708296f30c3c
Kazuki Atobe <kazuki.atobe@dena.jp>
2022-09-26 17:55:21 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     outputDir: 'docs',
-    publicPath: './',
+    publicPath: '/',
     filenameHashing: false,
     productionSourceMap: false,
     transpileDependencies: [
